7e71e71aef118caf7b8ddf66665c643a4e8e2b15
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-04 23:14:29 +0300

    From-The-Heart block redesign

--- src/components/Body/From-The-Heart/From-The-Heart.js
+++ src/components/Body/From-The-Heart/From-The-Heart.js
@@ -1,14 +1,25 @@
 import "./From-The-Heart.css";
-import icon from "../../Assets/photo_2023-11-04_23-14-44.jpg"
+import icon from "../../Assets/photo_2023-11-04_23-14-44.jpg";
 
 const FromTheHeart = () => {
   return (
-    <div className="for-the-heart-block">
-      <div className="for-the-heart-container">
-        <div className="for-the-heart__icon"><img src={icon}></img></div>
-        <div className="for-the-heart__content">
-          <div className="for-the-heart__title"><p>From The Heart</p></div>
-          <div className="for-the-heart__text"><p>Сотвори мир волшебства из мыслей, окрашенных в золото, и эмоций, окрашенных в цвета радуги.</p></div>
+    <div className="from-the-heart-block">
+      <div className="from-the-heart-container">
+        <div className="from-the-heart__icon">
+          <div className="from-the-heart__icon-container">
+            <img src={icon} alt="Heart icon" />
+          </div>
+        </div>
+        <div className="from-the-heart__content">
+          <div className="from-the-heart__title">
+            <p>From The Heart</p>
+          </div>
+          <div className="from-the-heart__text">
+            <p>
+              Сотвори мир волшебства из мыслей, окрашенных в золото, и эмоций,
+              окрашенных в цвета радуги.
+            </p>
+          </div>
         </div>
       </div>
     </div>
