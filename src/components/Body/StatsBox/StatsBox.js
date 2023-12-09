f9e620f94cc4cf7b77315e3d9031e842af3b4688
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-09 23:20:41 +0300

    Classes have been rewritten in accordance with the BEM

--- src/components/Body/StatsBox/StatsBox.js
+++ src/components/Body/StatsBox/StatsBox.js
@@ -3,15 +3,21 @@ import "./StatsBox.css";
 
 const StatsBox = () => {
   return (
-    <div className="stats-block">
-      <div className="stats-container">
-        <div className="stats-row">
-          <div className="stats-row__item"></div>
-          <div className="stats-row__item"></div>
+    <div className="stats-box">
+      <div className="stats-box__container">
+        <div className="stats-box__row">
+          <div className="stats-box__item">
+            <p className="stats-box__item-title"></p>
+            <p className="stats-box__item-content"></p>
+          </div>
+          <div className="stats-box__item"></div>
         </div>
-        <div className="stats-row">
-          <div className="stats-row__item"></div>
-          <div className="stats-row__item"></div>
+        <div className="stats-box__row">
+          <div className="stats-box__item"></div>
+          <div className="stats-box__item">
+            <p className="stats-box__item-title"></p>
+            <p className="stats-box__item-content"></p>
+          </div>
         </div>
       </div>
     </div>
