c90f35ce86818e3b05d5383539dffc53f427223a
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-09 23:54:12 +0300

    Redesign

--- src/components/Body/StatsBox/StatsBox.js
+++ src/components/Body/StatsBox/StatsBox.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./StatsBox.css";
+import rabbit from '../../Assets/rabbit.svg'
+import snake from '../../Assets/snake.svg'
 
 const StatsBox = () => {
   return (
@@ -7,16 +9,16 @@ const StatsBox = () => {
       <div className="stats-box__container">
         <div className="stats-box__row">
           <div className="stats-box__item">
-            <p className="stats-box__item-title"></p>
-            <p className="stats-box__item-content"></p>
+            <p className="stats-box__item-title">134</p>
+            <p className="stats-box__item-content">Дней с нашего знакомства</p>
           </div>
-          <div className="stats-box__item"></div>
+          <div className="stats-box__item"><img  src={snake}></img></div>
         </div>
         <div className="stats-box__row">
-          <div className="stats-box__item"></div>
+          <div className="stats-box__item"><img src={rabbit}></img></div>
           <div className="stats-box__item">
-            <p className="stats-box__item-title"></p>
-            <p className="stats-box__item-content"></p>
+            <p className="stats-box__item-title">1,904</p>
+            <p className="stats-box__item-content">Голосовых сообщений</p>
           </div>
         </div>
       </div>
