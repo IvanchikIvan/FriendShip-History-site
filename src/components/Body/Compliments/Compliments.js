18263c25b9d4934a5a8c4dba5b8540825a724189
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-11 20:48:35 +0300

    Added animations and redesing

--- src/components/Body/Compliments/Compliments.js
+++ src/components/Body/Compliments/Compliments.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { animated, useInView } from "react-spring";
 import "./Compliments.css";
 
 const Compliments = () => {
@@ -8,24 +9,41 @@ const Compliments = () => {
     "Ты определённо одна из самых лучших девушек, которых я встречал",
   ];
 
+  const [listRef, listStyles] = useInView(
+    () => ({
+      from: {
+        opacity: 0,
+        x: -1000,
+      },
+      to: {
+        opacity: 1,
+        x: 0,
+      },
+    }),
+    {
+      rootMargin: '-30% 0%',
+    }
+  )
+
   return (
-    <div className="compliments">
+    <div className="compliments" ref={listRef}>
       <div className="compliments-container">
         <div className="compliments__text-container">
           <div className="compliments__title-container">
             <p className="compliments__title">A Candy-Box of Compliments</p>
           </div>
-          <div className="compliments__content">
-            <ul className="compliments__list">
-              <div className="compliments__list-items">
-                {complimentsList.map((compliment, index) => (
-                  <li key={index} className="compliments__item">
-                    <p>{compliment}</p>
-                  </li>
-                ))}
-              </div>
-            </ul>
-          </div>
+          <animated.div className="compliments__content" style={listStyles}>
+              <ul className="compliments__list">
+                <div className="compliments__list-items">
+                  {complimentsList.map((compliment, index) => (
+                    <li key={index} className="compliments__item">
+                      <p>{compliment}</p>
+                    </li>
+                  ))}
+                </div>
+                <button></button>
+              </ul>
+          </animated.div>
         </div>
       </div>
     </div>
--- src/components/Body/From-The-Heart/From-The-Heart.js
+++ src/components/Body/From-The-Heart/From-The-Heart.js
@@ -1,14 +1,32 @@
 import "./From-The-Heart.css";
 import icon from "../../Assets/photo_2023-11-04_23-14-44.jpg";
+import { animated, useInView } from "react-spring";
 
 const FromTheHeart = () => {
+
+  const [iconRef, iconStyles] = useInView(
+    () => ({
+      from: {
+        opacity: 0,
+        y: 100,
+      },
+      to: {
+        opacity: 1,
+        y: 0,
+      },
+    }),
+    {
+      rootMargin: '-20% 0%',
+    }
+  )
+
   return (
     <div className="from-the-heart-block">
       <div className="from-the-heart-container">
         <div className="from-the-heart__icon">
-          <div className="from-the-heart__icon-container">
+          <animated.div className="from-the-heart__icon-container" ref={iconRef} style={iconStyles}>
             <img src={icon} alt="Heart icon" />
-          </div>
+          </animated.div>
         </div>
         <div className="from-the-heart__content">
           <div className="from-the-heart__title">
