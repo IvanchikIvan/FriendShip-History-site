d2c62444319ad2c0b6e20b03606ca34f033209e4
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-11 21:46:40 +0300

    Added animation and redesing in surprise block

--- src/components/Body/Surprise/Surprise.js
+++ src/components/Body/Surprise/Surprise.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { animated, useInView } from "react-spring";
 import "./Surprise.css";
 
 const Surprise = () => {
+  const [unboxRef, unboxStyles] = useInView(
+    () => ({
+      from: {
+        opacity: 0,
+        x: -100,
+      },
+      to: {
+        opacity: 1,
+        x: 0,
+      },
+    }),
+    {
+      rootMargin: '-30% 0%',
+    }
+  )
+
+  const [deliverRef, deliverStyles] = useInView(
+    () => ({
+      from: {
+        opacity: 0,
+        x: 100,
+      },
+      to: {
+        opacity: 1,
+        x: 0,
+      },
+    }),
+    {
+      rootMargin: '-30% 0%',
+    }
+  )
+
   return (
     <div className="surprise-block">
       <div className="surprise-container">
@@ -9,11 +42,18 @@ const Surprise = () => {
           <p>Surprise Now</p>
         </div>
         <div className="surprise-container__text">
-          <p>Сундук с сокровищами, наполненный комплиментами и пожеланиями. Зажгу радость в твоём сердце - сегодня и каждый день.</p>
+          <p>
+            Сундук с сокровищами, наполненный комплиментами и пожеланиями. Зажгу
+            радость в твоём сердце - сегодня и каждый день.
+          </p>
         </div>
         <div className="surprise-container__buttons-container">
-          <div className="surprise-container__unbox-happiness"><p>Unbox happiness</p></div>
-          <div className="surprise-container__deliver-delight"><p>Deliver Delight</p></div>
+          <animated.div className="surprise-container__unbox-happiness" ref={unboxRef} style={unboxStyles}>
+            <p>Unbox happiness</p>
+          </animated.div>
+          <animated.div className="surprise-container__deliver-delight" ref={deliverRef} style={deliverStyles}>
+            <p>Deliver Delight</p>
+          </animated.div>
         </div>
       </div>
     </div>
