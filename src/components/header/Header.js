e1164a619e045a2c101503c4828f5f510defb87a
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-12 23:49:38 +0300

    Added logo into Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,31 +1,40 @@
-import './Header.css'
-import { animated, useSpring, config } from 'react-spring';
+import "./Header.css";
+import { animated, useSpring, config } from "react-spring";
+import nastya_logo from "../Assets/nastya_logo_1.png";
 
 const Header = () => {
-
   const [beautyStyle] = useSpring(
     () => ({
-      from: { opacity: 0, y:-300, color: "#000" },
-      to: { opacity: 1, y:0, color: "#06A77D" },
-      config: config['molasses'],
+      from: { opacity: 0, y: -300, color: "#000" },
+      to: { opacity: 1, y: 0, color: "#06A77D" },
+      config: config["molasses"],
     }),
     []
-  )
+  );
 
   return (
-    <header className='header'>
-        <div className='navbar'>
-            <div className='for-you'><p>Для тебя, Настюша</p></div>
-            <div className='year'><p>2024</p></div>
+    <header className="header">
+      <div className="navbar">
+        <div className="for-you">
+          <div className="for-you-logo__container">
+            <img src={nastya_logo} alt=""></img>
+          </div>
+          <p>Для тебя, Настюша</p>
         </div>
-        <div className='header__body'>
-            <div className='header__title'>
-                <animated.p className='beauty' style={beautyStyle}>BEAUTY</animated.p>
-                <p className='for-you'>Для тебя, Настюша</p>
-            </div>
+        <div className="year">
+          <p>2024</p>
         </div>
+      </div>
+      <div className="header__body">
+        <div className="header__title">
+          <animated.p className="beauty" style={beautyStyle}>
+            BEAUTY
+          </animated.p>
+          <p className="for-you">Для тебя, Настюша</p>
+        </div>
+      </div>
     </header>
   );
-}
+};
 
 export default Header;
